106a9c0d3c2c58bcb538b2d5c4a1970e6dde999e
NickPodguzov993 <verytallman993@gmail.com>
2024-08-06 17:13:20 +0300

    'initial'

--- src/components/MainPage/Components/MoreBenefits.tsx
+++ src/components/MainPage/Components/MoreBenefits.tsx
@@ -1,4 +1,6 @@
 import {useState} from "react";
+import  back1 from '../../../../public/assets/MainPage/Mobile/back1.svg'
+import  notebook from '../../../../public/assets/MainPage/Mobile/notebook.png'
 
 
 const MoreBenefits = () => {
@@ -9,7 +11,7 @@ const MoreBenefits = () => {
             title:'Reliability',
             title2:'Augue ex',
             description:'Augue ex. Lacinia sed molestie libero, ultricies. Luctus dictum. Luctus mollis ornare velit molestie et efficitur nulla ex. Velit malesuad.',
-            img:'',
+            img:back1,
         },
         {
             id: 2,
@@ -58,11 +60,14 @@ const MoreBenefits = () => {
     };
 
     return (
-        <div>
+        <div className='w-[360px]'>
             <div className="w-[360px] h-[527px] px-4 flex-col justify-start items-start gap-8 inline-flex">
                 <div className="self-stretch h-[213px] flex-col justify-start items-center gap-4 flex">
                     <div className="#4cffa9">
-                        <div className="text-black text-sm font-normal font-['Arial'] leading-snug p-2 rounded-lg bg-[#4cffa9]">More benefits</div>
+                        <div
+                            className="text-black text-sm font-normal font-['Arial'] leading-snug p-2 rounded-lg bg-[#4cffa9]">More
+                            benefits
+                        </div>
                     </div>
                     <div className="w-[324px] justify-center items-center gap-3.5 ">
                         <div
@@ -79,10 +84,31 @@ const MoreBenefits = () => {
                         core strength
                     </div>
                 </div>
-                <div className="w-[328px] justify-start items-center gap-3.5 inline-flex">
 
-                </div>
             </div>
+            <div className="w-full  justify-start  items-start inline-flex  overflow-y-hidden">
+                {cards.map(c => (
+                    <div key={c.id} onTouchStart={() => touchStart(c.id)} onTouchEnd={() => touchEnd(c.id)}
+                         className={`w-[300px] h-[282px] p-[24px] bg-[#4cffa9] bg-[url(\`/assets/MainPage/Mobile/{c.img}\`)] bg-no-repeat
+                         bg-cover rounded-[20px] flex-col justify-between items-start ml-2 inline-flex`}>
+                        <div className="">
+                            <div className="w-[267px]  justify-center items-center inline-flex"/>
+                            <div className="self-stretch text-black text-xl font-normal font-['Arial'] mb-[30px]">{c.title}
+                            </div>
+                            <div className="self-stretch  flex-col justify-start items-start gap-5 flex">
+                                <div className="self-stretch text-black text-base font-normal font-['Arial']">{c.title2}
+                                </div>
+                                <div
+                                    className="self-stretch text-black text-sm font-normal font-['Arial'] leading-snug">{c.description}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+
+
 
             <div className=" px-4 flex-col justify-start items-start gap-8 inline-flex">
                 <div className="self-stretch"><span
@@ -94,7 +120,7 @@ const MoreBenefits = () => {
                 </div>
                 <div className="w-[328px] h-[212.09px] relative">
                     <img className="w-[328px] h-[212.09px] left-0 top-0 absolute"
-                         src="https://via.placeholder.com/328x212"/>
+                         src={notebook}/>
                     <div className="#f4f4f4"/>
                 </div>
                 <div>A ready-to-use online service, no special software installation actions are
--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -15,8 +15,7 @@ const MainPage = () => {
 
     return (
         <div
-            className='overflow-y-hidden w-full sm:pt-[150.5px] pt-[120px] flex flex-col justify-start items-center sm:gap-40 sm:gap-[120px]
-            animate-fade-down animate-once animate-duration-1000 animate-delay-300 animate-ease-linear animate-normal'>
+           >
             <About/>
             <MoreBenefits/>
             <Tools/>
