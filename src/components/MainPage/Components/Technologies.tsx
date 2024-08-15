85180eb2dbd9d36c666770bf271c394e655c036d
NickPodguzov993 <verytallman993@gmail.com>
2024-08-15 17:17:20 +0300

    initial

--- src/components/MainPage/Components/Technologies.tsx
+++ src/components/MainPage/Components/Technologies.tsx
@@ -1,13 +1,15 @@
 import table from '../../../../public/assets/MainPage/Mobile/tables.png'
 
+
 const Technologies = () => {
     return (
         <div className=' '>
             <div>
-                
+
             </div>
             <div className=" flex-col justify-center items-start gap-4 inline-flex">
-                <div className="bg-[url('/assets/MainPage/Mobile/half-round.svg')] bg-[top_-30px_right_-20px] bg-no-repeat w-[360px] h-[360px]">
+                <div
+                    className="bg-[url('/assets/MainPage/Mobile/half-round.svg')] bg-[top_-30px_right_-20px] bg-no-repeat w-[360px] h-[360px]">
                     <img className='w-[360px] mt-[10px]' src={table}/>
                 </div>
                 <div className="bg-[#4cffa9] p-2 ml-4  rounded-lg relative">
@@ -18,24 +20,108 @@ const Technologies = () => {
                     className="text-black text-[32px] font-light font-['Oswald'] uppercase">technologies</span><span
                     className="text-black text-[32px] font-medium font-['Oswald'] uppercase"> used for development</span>
                 </div>
-                <div className=" pl-4 w-[340px]">Our choice of technologies reflects our commitment to delivering top-tier
+                <div className=" pl-4 w-[340px]">Our choice of technologies reflects our commitment to delivering
+                    top-tier
                     solutions for businesses of all sizes
                 </div>
             </div>
-            <div
-                className="w-[328px] text-center  text-[40px] font-medium font-['Oswald'] uppercase">innovative
-                technology stack
-            </div>
-            <div className="#4cffa9">
-                <div className="#4cffa9">
-                    <div className="text-black text-sm font-normal font-['Arial'] leading-snug">Stack 01</div>
+            <div className="h-[169px] w-[360px] relative justify-start items-center inline-flex">
+                <div
+                    className="w-[169px]  h-[169px] absolute z-10 left-[90px] bg-black/40 bg-[url('/assets/MainPage/Mobile/Ellipse6.png')] rounded-full"/>
+                <div
+                    className="w-[328px] text-center text-white text-[40px] absolute z-20 left-[20px] font-medium font-['Oswald'] uppercase">innovative
+                    technology stack
                 </div>
-                <div className="self-stretch h-[67px] flex-col justify-start items-start gap-[9px] flex">
-                    <div className="self-stretch text-black text-2xl font-medium font-['Oswald'] uppercase">Java /
-                        Kotlin
-                    </div>
-                    <div
-                        className="self-stretch text-black text-sm font-normal font-['Arial'] leading-snug">(back-end)
+                <div
+                    className="w-[169px]  h-[169px] absolute   bg-black/10 left-[16px] bg-[url('/assets/MainPage/Mobile/Ellipse5.png')]  rounded-full"/>
+                <div
+                    className="w-[169px]  h-[169px] absolute  left-[185px] bg-[url('/assets/MainPage/Mobile/Ellipse7.png')] bg-black z-10 rounded-full"/>
+
+            </div>
+            <div className="w-[360px] h-[296px] flex-col justify-start items-start gap-8 inline-flex">
+
+                <div className=" w-full  justify-start items-start inline-flex flex-col overflow-y-hidden">
+                    <div className="flex flex-row h-[500px]   mt-[40px]">
+                        <div
+                            className="w-[300px] h-[196px] origin-top-left  flex-col justify-center items-center inline-flex">
+                            <div className="#4cffa9">
+                                <div className="#4cffa9">
+                                    <div className="text-black text-sm font-normal font-['Arial'] leading-snug">Stack
+                                        01
+                                    </div>
+                                </div>
+                                <div
+                                    className="self-stretch h-[67px] flex-col justify-start items-start gap-[9px] flex">
+                                    <div
+                                        className="self-stretch text-black text-2xl font-medium font-['Oswald'] uppercase">Java
+                                        / Kotlin
+                                    </div>
+                                    <div
+                                        className="self-stretch text-black text-sm font-normal font-['Arial'] leading-snug">(back-end)
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            className="w-[300px] h-[196px] origin-top-left  flex-col justify-center items-center inline-flex">
+                            <div className="#4cffa9">
+                                <div className="#4cffa9">
+                                    <div className="text-black text-sm font-normal font-['Arial'] leading-snug">Stack
+                                        01
+                                    </div>
+                                </div>
+                                <div
+                                    className="self-stretch h-[67px] flex-col justify-start items-start gap-[9px] flex">
+                                    <div
+                                        className="self-stretch text-black text-2xl font-medium font-['Oswald'] uppercase">Java
+                                        / Kotlin
+                                    </div>
+                                    <div
+                                        className="self-stretch text-black text-sm font-normal font-['Arial'] leading-snug">(back-end)
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            className="w-[300px] h-[196px] origin-top-left  flex-col justify-center items-center inline-flex">
+                            <div className="#4cffa9">
+                                <div className="#4cffa9">
+                                    <div className="text-black text-sm font-normal font-['Arial'] leading-snug">Stack
+                                        01
+                                    </div>
+                                </div>
+                                <div
+                                    className="self-stretch h-[67px] flex-col justify-start items-start gap-[9px] flex">
+                                    <div
+                                        className="self-stretch text-black text-2xl font-medium font-['Oswald'] uppercase">Java
+                                        / Kotlin
+                                    </div>
+                                    <div
+                                        className="self-stretch text-black text-sm font-normal font-['Arial'] leading-snug">(back-end)
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            className="w-[300px] h-[196px] origin-top-left flex-col justify-center items-center inline-flex">
+                            <div className="#4cffa9">
+                                <div className="#4cffa9">
+                                    <div className="text-black text-sm font-normal font-['Arial'] leading-snug">Stack
+                                        01
+                                    </div>
+                                </div>
+                                <div
+                                    className="self-stretch h-[67px] flex-col justify-start items-start gap-[9px] flex">
+                                    <div
+                                        className="self-stretch text-black text-2xl font-medium font-['Oswald'] uppercase">Java
+                                        / Kotlin
+                                    </div>
+                                    <div
+                                        className="self-stretch text-black text-sm font-normal font-['Arial'] leading-snug">(back-end)
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
